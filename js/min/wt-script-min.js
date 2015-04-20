4f125a0428745bea8fe1725c2b4c6a70caee509f
Jason Pamental <jason@freshtilledsoil.com>
2015-04-19 22:27:57 -0700

    fixing setup

--- js/min/wt-script-min.js
+++
@@ -1,1 +0,0 @@
-$(function(){wt.fix(),$(window).resize(function(){wt.fix()})}),wt.fix({elements:"p",chars:10,method:"nbsp",event:"resize"}),wt.fix({elements:"blockquote",chars:2,method:"nbsp",event:"resize"});
